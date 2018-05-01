eb8d4c52a20af766956eb6617461c4db50d7e492
Bart van Oort <bvanoort@chello.nl>
2018-05-01 23:58:44 +0200

    Adds brightness and roomlight sliders, on / off button

--- ceiled-web/src/App.js
+++ ceiled-web/src/App.js
@@ -4,19 +4,41 @@ import { CardTitle } from 'material-ui/Card';
 
 import LEDControls from './LEDControls';
 import Paper from 'material-ui/Paper/Paper';
+import IconButton from 'material-ui/IconButton';
+import PowerIcon from 'material-ui/svg-icons/action/power-settings-new';
+import { greenA700, redA700 } from 'material-ui/styles/colors';
 
 class App extends Component {
-    render() {
-        const mainStyle = {
-            width: this.props.isMobile ? this.props.windowSize.innerWidth : this.props.mobileMaxWidth
-        }
-        return (
-            <Paper style={mainStyle}>
-                <CardTitle title='CeiLED' subtitle='Controlling those LEDs on a ceiling near you ;)' />
-                <LEDControls />
-            </Paper>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      enabled: true
+    };
+  }
+  
+  render() {
+    const mainStyle = {
+      width: this.props.isMobile ? this.props.windowSize.innerWidth : this.props.mobileMaxWidth
     }
+    
+    return (
+      <Paper style={mainStyle}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <CardTitle title='CeiLED' subtitle='Controlling those LEDs on a ceiling near you ;)' />
+          <IconButton 
+            onClick={event => this.setState({ enabled: !this.state.enabled })}
+            iconStyle={{ height: 68, width: 68 }}
+            style={{ height: 84, width: 84, paddingRight: 16 }}
+          >
+            <PowerIcon 
+              color={this.state.enabled ? greenA700 : redA700 }
+            />
+          </IconButton>
+        </div>
+        <LEDControls enabled={this.state.enabled} />
+      </Paper>
+    );
+  }
 }
 
 export default withWindowSize(App);
--- ceiled-web/src/LEDControls.js
+++ ceiled-web/src/LEDControls.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardTitle, CardText } from 'material-ui/Card';
+import Slider from 'material-ui/Slider';
 
 import FadeControls from './fade/FadeControls';
 import FunControls from './fun/FunControls';
@@ -7,9 +8,49 @@ import JumpControls from './jump/JumpControls';
 import SolidControls from './solid/SolidControls';
 
 class LEDControls extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brightness: 100,
+      roomLight: 0
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.enabled) { 
+      this.setState({ brightness: 0 });
+    } else {
+      this.setState({ brightness: 100 });
+    }
+  }
+
   render() {
     return (
       <div>
+        <Card>
+          <CardText style={{ display: 'flex', flexFlow: 'column nowrap' }}>
+            Brightness: {this.state.brightness}
+            <Slider 
+              min={0} 
+              max={100}
+              step={1}
+              value={this.state.brightness}
+              disabled={!this.props.enabled}
+              onChange={(event, newValue) => this.setState({ brightness: newValue })}
+              sliderStyle={{ marginTop: 10, marginBottom: 10 }} 
+            />
+            Roomlight: {this.state.roomLight}
+            <Slider 
+              min={0} 
+              max={100}
+              step={1}
+              value={this.state.roomLight}
+              disabled={!this.props.enabled}
+              onChange={(event, newValue) => this.setState({ roomLight: newValue })}
+              sliderStyle={{ marginTop: 10, marginBottom: 10 }} 
+            />
+          </CardText>
+        </Card>
         <Card initiallyExpanded style={{ width: '100%' }} >
           <CardTitle actAsExpander showExpandableButton title="Solids" />
           <CardText expandable >
