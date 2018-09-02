2f06d117a5d4eda3a56ff26d53d1710cbb4b19bf
Bart van Oort <bvanoort@chello.nl>
2018-09-02 19:23:46 +0200

    Removes old Material-UI version, removes old commented code

--- ceiled-web/src/App.js
+++ ceiled-web/src/App.js
@@ -39,47 +39,6 @@ class App extends Component {
   }
 }
 
-// import { CardTitle } from 'material-ui/Card';
-
-// import LEDControls from './LEDControls';
-// import PaperV0 from 'material-ui/Paper/Paper';
-// import IconButton from 'material-ui/IconButton';
-// import PowerIcon from 'material-ui/svg-icons/action/power-settings-new';
-// import { greenA700, redA700 } from 'material-ui/styles/colors';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       enabled: true
-//     };
-//   }
-  
-//   render() {
-//     const mainStyle = {
-//       width: this.props.isMobile ? this.props.windowSize.innerWidth : this.props.mobileMaxWidth
-//     }
-    
-//     return (
-//       <Paper style={mainStyle}>
-//         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-//           <CardTitle title='CeiLED' subtitle='Controlling those LEDs on a ceiling near you ;)' />
-//           <IconButton 
-//             onClick={event => this.setState({ enabled: !this.state.enabled })}
-//             iconStyle={{ height: 68, width: 68 }}
-//             style={{ height: 84, width: 84, paddingRight: 16 }}
-//           >
-//             <PowerIcon 
-//               color={this.state.enabled ? greenA700 : redA700 }
-//             />
-//           </IconButton>
-//         </div>
-//         <LEDControls enabled={this.state.enabled} />
-//       </Paper>
-//     );
-//   }
-// }
-
 const StyledApp = compose(withStyles(styles), withWidth())(App);
 const HotStyledApp = hot(module)(() => 
   <MuiThemeProvider theme={theme}>
