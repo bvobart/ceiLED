53d6bae00115f0256e9b137ef11bc34de4f97a85
Bart van Oort <bart@vanoort.is>
2020-10-12 22:41:06 +0200

    Fail ceiled env upon not having $EDITOR defined

--- ceiled-cli/commands/env.sh
+++ ceiled-cli/commands/env.sh
@@ -4,5 +4,6 @@ function env {
   cd $CEILED_DIR
   [[ -d .env ]] && fail "$CEILED_DIR/.env is a directory, please remove it."
   [[ -f .env ]] || cp .env.sample .env
+  [[ -z "$EDITOR" ]] && fail "No default editor set. Please set the EDITOR environment variable, or manually edit $CEILED_DIR/.env with your favourite text editor."
   $EDITOR .env
 }
