679578b9445384e0d3641cf8a555ce4616fe82ce
Bart van Oort <bart@vanoort.is>
2020-11-18 00:05:56 +0100

    ceiled-cli: make update command able to repair CeiLED installations by fixing the tracked files

--- ceiled-cli/commands/update.sh
+++ ceiled-cli/commands/update.sh
@@ -5,6 +5,18 @@ function update {
   print_yellow "--> Fetching updates on $ref..."
   git pull origin $ref
 
+  # check whether there's any new files that need to be tracked and pulled
+  if [[ -f .git/info/sparse-checkout ]]; then
+    local known_files=$(cat .git/info/sparse-checkout)
+    local ceiled_files=$(echo_ceiled_files)
+
+    if [[ $ceiled_files != $known_files ]]; then
+      print_yellow "--> Fetching new files from $ref..."
+      echo_ceiled_files > .git/info/sparse-checkout
+      git pull origin $ref
+    fi
+  fi
+
   print_yellow "--> Fetching new Docker images"
   docker-compose pull
   print_green "--> Done!"
