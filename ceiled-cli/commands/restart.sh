b8e3a12b5b62877273e1543a2e9237f063af7b4b
Bart van Oort <bart@vanoort.is>
2020-12-23 00:04:31 +0100

    ceiled-cli: removes rolling update from restart as that was actually quite slow and hardly necessary

--- ceiled-cli/commands/restart.sh
+++ ceiled-cli/commands/restart.sh
@@ -1,72 +1,35 @@
-# Scales a service $1 to have $2 replicas.
-function scale_service {
-  docker-compose up -d --no-deps --scale $1=$2 $1
+# Restarts one or more CeiLED services without a rolling update, simply using docker-compose up -d --no-deps.
+function restart_services {
+  docker-compose up -d --no-deps $@
 }
 
-# Restarts a single CeiLED service with name $1 by doing a rolling update.
-# Expects that $1 is one of either [server, driver, mongodb].
-function restart_service {
-  local container_id=$(docker-compose ps -q $1)
-  print_yellow "--> Starting new instance of ceiled-$1..."
-  [[ -n "$container_id" ]] && scale_service $1 2 || scale_service $1 1
-  sleep 1
-
-  if [[ -n "$container_id" ]]; then
-    print_yellow "--> Removing old instance of ceiled-$1..."
-    docker kill -s SIGTERM $container_id &> /dev/null || true
-    sleep 1
-    docker rm -f $container_id &> /dev/null || true
-  fi
-
-  [[ -n "$container_id" ]] && scale_service $1 1
-}
-
-# Restarts ceiled-web without a rolling update, as it binds to ports on the host.
-function restart_web {
-  docker-compose up -d --no-deps web
-}
-
-# Restarts all CeiLED service by doing a rolling update, except for ceiled-web.
+# Restarts all CeiLED services.
 function restart_all {
-  local driver_id=$(docker-compose ps -q driver)
-  local mongodb_id=$(docker-compose ps -q mongodb)
-  local server_id=$(docker-compose ps -q server)
+  restart_services server driver mongodb web
+}
 
-  print_yellow "--> Starting new services..."
-  [[ -n "$driver_id" ]] && scale_service driver 2 || scale_service driver 1
-  [[ -n "$mongodb_id" ]] && scale_service mongodb 2 || scale_service mongodb 1
-  [[ -n "$server_id" ]] && scale_service server 2 || scale_service server 1
-  restart_web
-  sleep 1
-
-  if [[ -n "$driver_id" ]] || [[ -n "$mongodb_id" ]] || [[ -n "$server_id" ]]; then
-    print_yellow "--> Removing old services..."
-    docker kill -s SIGTERM $driver_id $mongodb_id $server_id &> /dev/null || true
-    sleep 1
-    docker rm -f $driver_id $mongodb_id $server_id &> /dev/null || true
-  fi
-  
-  [[ -n "$driver_id" ]] && scale_service driver 1
-  [[ -n "$mongodb_id" ]] && scale_service mongodb 1
-  [[ -n "$server_id" ]] && scale_service server 1
-  true
+# Checks if $1 is a CeiLED service.
+function is_ceiled_service {
+  [[ "$1" == "server" || "$1" == "driver" || "$1" == "mongodb" || "$1" == "web" ]]
 }
 
 # Restarts CeiLED if it is running, or starts it if it doesn't.
 function restart {
   cd $CEILED_DIR
 
-  if [[ "$1" == "server" || "$1" == "driver" || "$1" == "mongodb" ]]; then
-    print_yellow "--> Restarting ceiled-$1..."
-    restart_service "$1"
-  elif [[ "$1" == "web" ]]; then
-    print_yellow "--> Restarting ceiled-web..."
-    restart_web
-  else
-    # by default, restart all services
+  # when called without further arguments, restart all services
+  if [[ $# == 1 || "$1" == "all" ]]; then
     print_yellow "--> Restarting all CeiLED's services..."
-    restart_all 
+    restart_all
+    print_green "--> Done!"
+    exit 0
   fi
+
+  # when called with one or more args, check whether they're all valid CeiLED services, then restart them
+  for arg in $@; do
+    is_ceiled_service "$arg" || fail "argument must be one or more of [server, driver, mongodb, web], but got: $arg"
+  done
+  restart_services $@
 
   print_green "--> Done!"
 }
