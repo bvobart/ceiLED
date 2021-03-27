49889e122d015de03d63213d6d5b86c085928c1c
Bart van Oort <bart@vanoort.is>
2021-03-27 15:16:15 +0100

    Adds withCeiled HOC to collect and apply all CeiLED's context providers

--- ceiled-web/src/App.tsx
+++ ceiled-web/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Container, Grid, useMediaQuery, Theme } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import Header from './components/Header';
@@ -8,8 +8,7 @@ import GlobalControls from './controls/GlobalControls';
 import MoodControls from './controls/MoodControls';
 import SolidControls from './controls/SolidControls';
 import AnimationControls from './controls/AnimationControls';
-import { SocketProvider } from './hooks/context/SocketContext';
-import { StatusProvider } from './hooks/context/StatusContext';
+import { withCeiled } from './hooks/context/withCeiled';
 
 const useStyles = makeStyles<Theme>(theme => ({
   root: {
@@ -55,52 +54,48 @@ const useStyles = makeStyles<Theme>(theme => ({
   },
 }));
 
-const App = (): JSX.Element => {
+const App: FC = () => {
   const classes = useStyles();
   const isMedium = useMediaQuery<Theme>(theme => theme.breakpoints.up('md'));
   const isLarge = useMediaQuery<Theme>(theme => theme.breakpoints.up('lg'));
 
   return (
-    <SocketProvider>
-      <StatusProvider>
-        <Container className={classes.root} disableGutters maxWidth={isMedium && !isLarge ? 'md' : false}>
-          <ErrorDialog />
+    <Container className={classes.root} disableGutters maxWidth={isMedium && !isLarge ? 'md' : false}>
+      <ErrorDialog />
 
-          <Grid className={classes.main} container>
-            <Grid container item xs={12} lg={6} alignContent='flex-start'>
-              <Grid item xs={12} className={classes.header}>
-                <Header />
-                <GlobalControls />
-              </Grid>
-              <Grid className={classes.solids} item xs={12}>
-                <SolidControls expanded={isLarge} />
-              </Grid>
-              {isLarge && (
-                <Grid item xs={12}>
-                  <Footer className={classes.footer} />
-                </Grid>
-              )}
-            </Grid>
-
-            <Grid container item xs={12} lg={6} alignContent='flex-start'>
-              <Grid className={classes.moods} item xs={12}>
-                <MoodControls expanded={isLarge} />
-              </Grid>
-              <Grid className={classes.animations} item xs={12}>
-                <AnimationControls expanded={isLarge} />
-              </Grid>
-            </Grid>
-
-            {!isLarge && (
-              <Grid item xs={12}>
-                <Footer className={classes.footer} />
-              </Grid>
-            )}
+      <Grid className={classes.main} container>
+        <Grid container item xs={12} lg={6} alignContent='flex-start'>
+          <Grid item xs={12} className={classes.header}>
+            <Header />
+            <GlobalControls />
           </Grid>
-        </Container>
-      </StatusProvider>
-    </SocketProvider>
+          <Grid className={classes.solids} item xs={12}>
+            <SolidControls expanded={isLarge} />
+          </Grid>
+          {isLarge && (
+            <Grid item xs={12}>
+              <Footer className={classes.footer} />
+            </Grid>
+          )}
+        </Grid>
+
+        <Grid container item xs={12} lg={6} alignContent='flex-start'>
+          <Grid className={classes.moods} item xs={12}>
+            <MoodControls expanded={isLarge} />
+          </Grid>
+          <Grid className={classes.animations} item xs={12}>
+            <AnimationControls expanded={isLarge} />
+          </Grid>
+        </Grid>
+
+        {!isLarge && (
+          <Grid item xs={12}>
+            <Footer className={classes.footer} />
+          </Grid>
+        )}
+      </Grid>
+    </Container>
   );
 };
 
-export default App;
+export default withCeiled(App);
---
+++ ceiled-web/src/hooks/context/withCeiled.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import { BrightnessProvider } from './BrightnessContext';
+import { SocketProvider } from './SocketContext';
+import { StatusProvider } from './StatusContext';
+
+/**
+ * withCeiled is a higher-order (functional) component made to wrap the wrapped component
+ * in the context providers that CeiLED uses.
+ * @param Component your main App component
+ */
+export function withCeiled<P>(Component: FC<P>): FC<P> {
+  return function WithCeiled(props: P) {
+    return (
+      <SocketProvider>
+        <StatusProvider>
+          <BrightnessProvider>
+            <Component {...props} />
+          </BrightnessProvider>
+        </StatusProvider>
+      </SocketProvider>
+    );
+  };
+}
